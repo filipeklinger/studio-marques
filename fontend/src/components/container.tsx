c810980bde6d9b507d3c207d430de7a23446d46d
Filipe klinger <filipeklinger@gmail.com>
2023-12-21 20:55:33 -0300

    Update container component styling

--- fontend/src/components/container.tsx
+++ fontend/src/components/container.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 
 export function Container({children}: {children:React.ReactNode}){
+    //bg-slate-400
     return (
-    <div className="bg-slate-400 text-white min-h-screen">
+    <div className="bg-black text-white min-h-screen text-center tracking-tighter md:tracking-normal">
         {children}
     </div>)
 }
