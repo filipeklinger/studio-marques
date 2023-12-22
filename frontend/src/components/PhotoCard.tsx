4c9819b80ebe8433d447d238842ff94e2447d9bd
Filipe klinger <filipeklinger@gmail.com>
2023-12-22 16:35:07 -0300

    Add onClick event to PhotoCard component

--- frontend/src/components/PhotoCard.tsx
+++ frontend/src/components/PhotoCard.tsx
@@ -1,9 +1,9 @@
-export function PhotoCard({ src, children }: { src: string, children: React.ReactNode }) {
+export function PhotoCard({ src, children, onClick }: { src: string, children: React.ReactNode, onClick?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void }) {
     return (
         <div className="flex-1 relative  w-full aspect-[3/4]">
             <img className="absolute w-full h-full object-cover" src={src} />
             <div className="absolute flex justify-center items-center w-full h-full ">
-                <div className="rounded-3xl flex flex-col justify-center items-center w-48 h-48 text-white bg-black/[.5]">
+                <div onClick={(e) => onClick?.(e)} className="rounded-3xl flex cursor-pointer flex-col justify-center items-center w-48 h-48 text-white bg-black/[.5] hover:bg-black/80">
                     {children}
                 </div>
 
